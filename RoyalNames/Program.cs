b86e0ccda11dc6b256cf74c42fc3f25080b55ff5
Marko Žlogar <marko.zlogar@gmail.com>
2016-07-04 07:41:46 +0200

    SolutionCommit
    Commit of solution

---
+++ RoyalNames/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoyalNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[]
+            {
+                "Fillip XI",
+                "Fillip IV",
+                "Fillip III",
+                "Fillipe I"
+            };
+            Array.Sort(names, new AlphanumComparator());
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.Read();
+        }
+    }
+}
